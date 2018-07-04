ce5222072b592d6b2006f48c65fb8978ab711252
James Lefty Larimer <lefty1044@gmail.com>
2018-07-04 08:05:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function add (a,b){
   return 
 }
+function subtract (a,b){
+  return 
+}
+function multiply (a,b){
+  return 
+}
+function divide (a,b){
+  return 
+}
+function inc (a,b){
+  return 
+}
